5f6eeb84fd8c81d35a16cf21e15850c94b5c84cc
duriskifeb <rizkydurfeb976@gmail.com>
2025-04-06 00:34:49 +0700

    hero Done tgl ubh design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Hero from "./component/Hero";
 
 function App() {
   return (
     <>
-      <p>sorry to say</p>  
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,16 +1,16 @@
 //hero ini saya copas nggeh
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/Design uten navn.png";
-import sun from "../../assets/sun.svg";
-import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
-import githubLight from "../../assets/github-light.svg";
-import githubDark from "../../assets/github-dark.svg";
-import linkedinLight from "../../assets/linkedin-light.svg";
-import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/cv.pdf";
-import { useTheme } from "../../common/ThemeContext";
+import heroImg from "../assets/Design uten navn.png";
+import sun from "../assets/sun.svg";
+import moon from "../assets/moon.svg";
+import twitterLight from "../assets/twitter-light.svg";
+import twitterDark from "../assets/twitter-dark.svg";
+import githubLight from "../assets/github-light.svg";
+import githubDark from "../assets/github-dark.svg";
+import linkedinLight from "../assets/linkedin-light.svg";
+import linkedinDark from "../assets/linkedin-dark.svg";
+import CV from "../assets/cv.pdf";
+import { useTheme } from "../common/ThemeContext";
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
