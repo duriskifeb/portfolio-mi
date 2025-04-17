aa7db92c23c35a67386ee5306aca2dcefb8680fe
duriskifeb <rizkydurfeb976@gmail.com>
2025-04-17 20:22:48 +0700

    done deploy ke gh-page github

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio-mi/", // <== wajib buat GitHub Pages!
+  base: "/portfolio-mi/", // <--- PENTING!
   plugins: [react()],
 });
