cd2471bbab263eabcee55b7ee00172d7a87b8b8d
duriskifeb <rizkydurfeb976@gmail.com>
2025-04-17 19:44:46 +0700

    re2 deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-mi/',
+  base: "/portfolio-mi/", // <== wajib buat GitHub Pages!
   plugins: [react()],
-})
+});
